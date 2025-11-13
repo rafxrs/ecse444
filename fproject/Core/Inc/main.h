1b0260f67b16ca1daa8c7d9b387aadf198c01519
Rafael Reis <rafael.reis@mail.mcgill.ca>
2025-11-12 20:05:07 -0500

    completed milestone 1

--- fproject/Core/Inc/main.h
+++ fproject/Core/Inc/main.h
@@ -57,6 +57,11 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define BUTTON_Pin GPIO_PIN_13
+#define BUTTON_GPIO_Port GPIOC
+#define BUTTON_EXTI_IRQn EXTI15_10_IRQn
+#define LED_Pin GPIO_PIN_14
+#define LED_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
